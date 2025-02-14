114a4b4c5493a459e02f3d64f2296d8320f75160
Marcolo24 <100005655.joan23@fje.edu>
2025-02-14 18:15:46 +0100

    Estilos y plantillas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,17 +4,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi Aplicación</title>
+    <title>@yield('title')</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap');
+    </style>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}"> <!-- Aquí se carga el CSS -->
 </head>
+<header>
+    <div>
+        <h1 id="titulo-principal">BCNFoodieGuide</h1>
+    </div>
+    <div>
+    {{-- Hay que colocar los links de las rutas --}}
+    <a href="">
+        <img class="img-header" src="{{ asset('images/lista.png') }}" alt="">
+    </a>
+    <a href="">
+        <img class="img-header" src="{{ asset('images/lupa.png') }}" alt="">
+    </a>
+    <a href="">
+    <img class="img-header" src="{{ asset('images/sesion.png') }}" alt="">
+    </a>
+    </div>
+</header>
 <body>
-
-    <header>
-        <h1>Bienvenidos a la Guía de Restaurantes</h1>
-    </header>
-
     <main>
         @yield('content') <!-- Aquí se inyectará el contenido de las vistas hijas -->
     </main>
-
 </body>
 </html>
